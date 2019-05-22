11be93e877b561d97b402d7806c51683cb432144
yanmei <yanmei@tsign.cn>
2019-05-22 23:30:19 +0800

    循环链表、双向链表

---
+++ src/my/list/CircularLinkedList/CircularLinkedList.java
@@ -0,0 +1,42 @@
+package my.list.CircularLinkedList;
+
+import my.list.LList;
+
+/*
+ * @author guanzheming
+ * @date 2019.05.22
+ * 循环链表简单的实现一下结构，不打算具体实现功能
+ * */
+public class CircularLinkedList implements LList{
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(int i, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object delete(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
